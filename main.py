ae0e6c6c105dfa5a62aec38d408fe4046cc7b17b
Jakub Kempa <jak.kempa@gmail.com>
2026-05-30 12:09:04 +0200

    `main.py` cleanup (had importing mistakes)

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from pathlib import Path
-from 
 
 from utils.load_data import load_data
 from utils.models import GLMdenoiser
-import utils.glmdenoise_viz as viz
 
 BASE_DIR = Path.cwd()
 DATA_DIR = BASE_DIR / "data"
@@ -18,8 +16,3 @@ if __name__ == "__main__":
 
     glm = GLMdenoiser(*all_data)
     result = glm.full_workflow(max_noise=10, n_boot=100, runs_num=len(runs_to_analyze))
-    
-    for run_idx in runs_to_analyze:
-        figure_path = FIGURES_DIR / f"tSNR-run{run_idx}"
-        viz.compute_tsnr(result, run_idx=run_idx, save_path=figure_path)
-
