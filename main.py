5f3f4aa5054ed6ea7327f2f04af64c748d9715f0
Jakub Kempa <jak.kempa@gmail.com>
2026-08-26 13:30:23 +0200

    added mask creation, loading and applying

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import argparse
 from pathlib import Path
 
 from general_linear_model.pipeline import PCADenoisingPipeline, PCADenoisingResult
+from independent_component_analysis.pipeline import ICADenoisingPipeline
 from utils.load_data import dataclass_from_pickle, dataclass_to_pickle, load_data
 
 BASE_PATH = Path.cwd() / "data" / "ds000105"
 
 
 def run_glm_pca_pipeline(subject):
-    runs, _ = load_data(
+    runs, _, _ = load_data(
         base_path=BASE_PATH,
         subject=subject
     )
@@ -30,16 +31,39 @@ def load_glm_pca_results(subject):
     
     return final_fit
 
+def run_ica_pipeline(subject):
+    runs, _, _ = load_data(
+        base_path=BASE_PATH,
+        subject=subject
+    )
+    
+    pipeline = ICADenoisingPipeline(
+        threshold=0.0,
+        n_permutations=1000,
+        tolerance=1e-5,
+        max_iter=1000,
+        high_pass_cutoff=128.0,
+        chunk_size=5000,
+        random_state=42,
+        verbose=False
+    )
+    result = pipeline.fit(runs)
+
+    pickle_path = Path.cwd() / "results" / f"ica_result_{subject}.pkl"
+    dataclass_to_pickle(result, pickle_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--subject", type=str, required=True)
     parser.add_argument("--run_glm", action="store_true")
     parser.add_argument("--load_result", action="store_true")
+    parser.add_argument("--run_ica", action="store_true")
     args = parser.parse_args()
 
     if args.run_glm:
         run_glm_pca_pipeline(subject=args.subject)
     elif args.load_result:
         result = load_glm_pca_results(subject=args.subject)
+    if args.run_ica:
+        run_ica_pipeline(subject=args.subject)
     
--- utils/load_data.py
+++ utils/load_data.py
@@ -5,8 +5,48 @@ from pathlib import Path
 import nibabel as nib
 import numpy as np
 import pandas as pd
+from nilearn.masking import apply_mask, compute_multi_epi_mask
 
 from utils.constants import RawData
+
+
+def _load_raw_imgs(dir_path):
+    """Load raw BOLD signals"""
+    
+    fmri_path = dir_path / "func"
+    fmri_files = sorted(fmri_path.glob("*_bold.nii.gz"))
+    raw_images = [nib.load(str(y)) for y in fmri_files]
+    return raw_images
+
+def _load_events_dfs(dir_path):
+    """Load events.tsv files as DataFrames"""
+    
+    fmri_path = dir_path / "func"
+    events_files = sorted(fmri_path.glob("*_events.tsv"))
+    events_dfs = [pd.read_csv(ev, sep="\t") for ev in events_files]
+    return events_dfs
+
+def _load_t1_img(dir_path):
+    t1_path = dir_path / "anat"
+    t1_file = next(t1_path.glob("*_T1w.nii.gz"))
+    t1_img = nib.load(t1_file)
+    return t1_img
+
+def _load_mask_imgs(dir_path):
+    mask_file = dir_path / "mask.nii.gz"
+    
+    if not mask_file.is_file():
+        _create_masks(dir_path)
+    
+    mask_img = nib.load(mask_file)
+    return mask_img
+
+def _create_masks(dir_path):
+    raw_imgs = _load_raw_imgs(dir_path)
+    mask_img = compute_multi_epi_mask(raw_imgs)
+    
+    mask_path = dir_path / "mask.nii.gz"
+    nib.save(mask_img, mask_path)
 
 
 def load_data(base_path, subject):
@@ -16,7 +56,8 @@ def load_data(base_path, subject):
     i.e. `load_data("~/Data/ds000105", "sub-1")`
 
     Returns:
-        - list of RunData dataclasses containing 2D fMRI data and events_df
+        - list of RunData dataclasses containing 2D masked fMRI data and events_df
+        - raw mask image
         - T1 high-resolution image
     """
     base_path = Path(base_path)
@@ -28,28 +69,22 @@ def load_data(base_path, subject):
     if not dir_path.exists():
         raise FileNotFoundError(f"{dir_path} does not exist")
 
-    # BOLD signals with events file
-    fmri_path = dir_path / "func"
-    fmri_files = sorted(fmri_path.glob("*.nii.gz"))
-    events_files = sorted(fmri_path.glob("*.tsv"))
-    
-    # T1 images
-    t1_path = dir_path / "anat"
-    t1_file = next(t1_path.iterdir())
-    t1_img = nib.load(t1_file)
+    raw_images = _load_raw_imgs(dir_path)
+    events_dfs = _load_events_dfs(dir_path)
+    mask_img = _load_mask_imgs(dir_path)
+    t1_img = _load_t1_img(dir_path)
     
     runs = []
-    
-    for y, ev in zip(fmri_files, events_files):
-        image = nib.load(str(y))
-        run = RawData(
-            Y=img_to_2d(image),
-            events_df=pd.read_csv(ev, sep="\t")
-        )
+    for img, ev in zip(raw_images, events_dfs):
+        Y = apply_mask(img, mask_img)
         
+        run = RawData(
+            Y=Y.astype(np.float32),
+            events_df=ev
+        )
         runs.append(run)
     
-    return runs, t1_img
+    return runs, mask_img, t1_img
 
 def img_to_2d(img):
     data = img.get_fdata(dtype=np.float32)
